2018efad054aaec4a3e0634193c156020e23483e
SINGH 1258 <sumantpaldm@gmail.com>
2021-12-08 13:06:15 -0500

    added home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Slider from "../components/Slider.jsx";
+import Categories from "../components/Categories.jsx";
+import Announcement from "../components/Announcement.jsx";
+import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
+
+const Home = () => {
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+      <Slider />
+      <Categories />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
