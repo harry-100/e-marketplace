8cc542ab67abbccbbb0f9427c9ef7a9b53db85cc
SINGH 1258 <sumantpaldm@gmail.com>
2021-12-08 13:09:17 -0500

    added product page

---
+++ client/src/pages/Product.jsx
@@ -0,0 +1,101 @@
+import styled from "styled-components";
+import React from "react";
+import Announcement from "../components/Announcement";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+  border: 3px solid black;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+
+const Button = styled.button`
+  margin-top: 20px;
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+  border-radius: 20%;
+  &:hover {
+    background-color: green;
+  }
+`;
+
+const Product = () => {
+  return (
+    <Container>
+      <Announcement />
+      <Navbar />
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Desc>
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            venenatis, dolor in finibus malesuada, lectus ipsum porta nunc, at
+            iaculis arcu nisi sed mauris. Nulla fermentum vestibulum ex, eget
+            tristique tortor pretium ut. Curabitur elit justo, consequat id
+            condimentum ac, volutpat ornare.
+          </Desc>
+          <Price>$ 20</Price>
+
+          <AddContainer>
+            <AmountContainer></AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
+};
+
+export default Product;
