5633e0a56f181bd8993131077bf6313c3d017c0c
Harry Shah <harvinder.shah@gmail.com>
2021-12-08 15:02:41 -0500

    user routes updated

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,6 +8,7 @@ router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
+    city: req.body.city,
     password: CryptoJS.AES.encrypt(
       req.body.password,
       process.env.PASS_SEC
