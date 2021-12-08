a8d4d28142eb3bede5bf108263c5713c68edb8bd
SINGH 1258 <sumantpaldm@gmail.com>
2021-12-08 14:12:52 -0500

    modified register form and package.json file

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -60,12 +60,15 @@ const Register = () => {
       <Wrapper>
         <Title>CREATE AN ACCOUNT</Title>
         <Form>
-          <Input placeholder="name" />
-          <Input placeholder="last name" />
-          <Input placeholder="username" />
-          <Input placeholder="email" />
-          <Input placeholder="password" />
-          <Input placeholder="confirm password" />
+          <Input placeholder="USERNAME" />
+          <Input placeholder="EMAIL" />
+          <Input
+            type="password"
+            minlength="8"
+            required
+            placeholder="PASSWORD"
+          />
+          <Input placeholder="CITY" />
           <Agreement>
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
